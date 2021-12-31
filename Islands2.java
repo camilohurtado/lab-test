95bf3baea32ccaf483c9eb55b22f673a52d39b4a
Camilo Hurtado <camilohurtadocarvajal@hotmail.com>
2021-12-31 17:54:32 -0500

    Finish solution

--- Islands2.java
+++ Islands2.java
@@ -3,6 +3,10 @@
  */
 public class Islands2 {
 
+    static int counter = 0;
+    static int global = 0;
+    static boolean[][] visited;
+
     /**
      * You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected
      * 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
@@ -11,85 +15,74 @@ public class Islands2 {
      **/
 
     public static void main(String[] args) {
-        int grid1[][] = { { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
-                { 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 0, 0
-                , 1, 1, 0, 0, 1, 0, 1, 0, 0 }, { 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 } };
-        int grid2[][] = { { 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1 }, { 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
-                { 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1 }, { 0, 1, 0, 0
-                , 1, 1, 0, 0, 0, 1, 0, 0, 1 }, { 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 } };
-        int grid3[][] = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0
-                , 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
+        int grid1[][] = {
+                { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+                { 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0 },
+                { 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 }
+        };
 
-        System.out.println("Is test case 1 passing? " + (maxAreaOfIsland(grid1) == 6));
-        System.out.println("Is test case 2 passing? " + (maxAreaOfIsland(grid2) == 12));
-        System.out.println("Is test case 3 passing? " + (maxAreaOfIsland(grid2) == 0));
+        int grid2[][] = {
+                { 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1 },
+                { 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+                { 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1 },
+                { 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1 },
+                { 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 }
+        };
+
+        int grid3[][] = {
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+        };
+
+        System.out.println("Is test case 1 passing? " + (maxAreaOfIsland(grid1) == 6) + "Actual result = " + (maxAreaOfIsland(grid1)));
+        System.out.println("Is test case 2 passing? " + (maxAreaOfIsland(grid2) == 12) + "Actual result = " + (maxAreaOfIsland(grid2)));
+        System.out.println("Is test case 3 passing? " + (maxAreaOfIsland(grid3) == 0) + "Actual result = " + (maxAreaOfIsland(grid3)));
     }
 
     public static int maxAreaOfIsland(int[][] grid) {
         Integer area = 0;
 
+        visited = new boolean[grid.length][grid[0].length];
+
         /////////////////////////
         //Insert your code here//
         /////////////////////////
-        int counter = 0;
-        int pos = -1; //Position where should look for 1s in next array
-
-        for(int i = 0; i < grid[i].length; i++){
-            int [] n = grid[i];
-
-            for (int j = 0; j < n.length; j ++){
-                int position = n[j];
-
-                if(position == 1){
-                    counter ++;
-                    //Look to the right
-                    if(j < n.length){
-                        int rightPosition = n[j + 1];
-                        if(rightPosition == 1){
-                            counter ++;
-                        }
-                    }
-
-                    //Look left position
-                    if(j > 0){
-                        int leftPosition = n[j - 1];
-                        if(leftPosition == 1){
-                            counter ++;
-                        }
-                    }
-
-                    //Look up
-                    if(i > 0){
-
-                    }
-                }
+        for (int i = 0; i < grid.length; i ++){
+            for (int j = 0; j < grid[i].length; j++) {
+                area = Math.max(area, area(i, j, grid));
             }
 
-
         }
-
         return area;
     }
 
-    static void searchForLand(int fromPosition, int counter, int[] portion) {
-        for (; fromPosition < portion.length; fromPosition++) {
-            int selectedNum = portion[fromPosition];
-            if (selectedNum == 1) {
-                counter++;
-            }
-
-            if (fromPosition < portion.length && portion[fromPosition] == 1) {
-                continue;
-            } else {
-                break;
-            }
+    public static int area(int i, int j,  int[][] grid){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || visited[i][j] || grid[i][j] == 0){
+            return 0;
+        }else{
+            visited[i][j] = true;
+            return (1 + //Go down
+                        area(i + 1, j, grid) +
+                        //Go up
+                        area(i - 1, j , grid) +
+                        //Go right
+                        area(i, j +1 , grid) +
+                        //Go left
+                        area(i, j -1, grid));
         }
     }
 }
